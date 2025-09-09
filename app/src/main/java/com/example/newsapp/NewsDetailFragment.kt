510524199f57d6662936c453e3c081a9728385cf
skr090850 <skr090850@gmail.com>
2025-09-09 11:24:37 +0530

    Complete ui and logic

--- app/src/main/java/com/example/newsapp/NewsDetailFragment.kt
+++ app/src/main/java/com/example/newsapp/NewsDetailFragment.kt
@@ -1,20 +1,43 @@
-package com.example.newsapp
+package com.example.newsapp.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
 
-class NewsDetailFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.fragment_news_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class NewsDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val webView: WebView = view.findViewById(R.id.web_view)
+        val progressBar: ProgressBar = view.findViewById(R.id.detail_progress_bar)
+        val url = arguments?.getString("article_url")
+
+        webView.apply {
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    progressBar.visibility = View.GONE
+                }
+            }
+            settings.javaScriptEnabled = true // Enable JavaScript for modern websites
+            url?.let {
+                loadUrl(it)
+            }
         }
     }
 }
